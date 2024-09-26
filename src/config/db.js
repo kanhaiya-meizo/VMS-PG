68a96da226290627f5b157729778fa6771329319
seetap-irique <seeta_p@iriquehitech.com>
2024-09-26 13:16:16 +0530

    Your commit message here

--- src/config/db.js
+++ src/config/db.js
@@ -10,9 +10,9 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_HOST);
-console.log(process.env.DB_NAME);
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_HOST);
+// console.log(process.env.DB_NAME);
 
 
 const connectToDB = async() => {
